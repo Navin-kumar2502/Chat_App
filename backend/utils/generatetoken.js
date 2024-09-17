ca18c78b5126f75bae26ecdde0dcfa9a534528b0
Navin-kumar2502 <114897504+Navin-kumar2502@users.noreply.github.com>
2024-09-17 18:24:27 +0530

    Update generatetoken.js

--- backend/utils/generatetoken.js
+++ backend/utils/generatetoken.js
@@ -3,7 +3,6 @@ const generateTokenandCookies = (userId, res) => {
     const token = jwt.sign({ userId },"sYxjmhAnShXdM1y1+Pe92syln17EfVHz8mVtvKcx3bU=", {
         expiresIn: '15d'
     });
-    console.log(userId)
     res.cookie("jwt", token, {
         maxAge: 15 * 24 * 60 * 60 * 1000,
         httpOnly: true,
